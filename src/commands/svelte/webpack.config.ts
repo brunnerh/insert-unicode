22b9c9c94aa23b09cbad00be687979236855d9e1
Harald Brunner <brunnerh@users.noreply.github.com>
2021-06-15 01:55:55 +0200

    Shorten chunk names in production build. Fixes #26.

--- src/commands/svelte/webpack.config.ts
+++ src/commands/svelte/webpack.config.ts
@@ -1,61 +1,64 @@
 import type { Configuration } from 'webpack';
 const sveltePreprocess = require('svelte-preprocess');
-
 const path = require('path');
 
-module.exports = (env: any, argv: Configuration) => <Configuration>{
-	entry: {
-		'manage-favorites/view': ['../manage-favorites/view.js'],
-		'data-table/view': ['../data-table/view.js'],
-	},
-	resolve: {
-		alias: {
-			svelte: path.resolve(__dirname, '..', '..', '..', 'node_modules', 'svelte')
+module.exports = (env: any, argv: Configuration) => {
+	const isProduction = argv.mode === 'production';
+
+	return <Configuration>{
+		entry: {
+			'manage-favorites/view': ['../manage-favorites/view.js'],
+			'data-table/view': ['../data-table/view.js'],
 		},
-		extensions: ['.svelte', '.ts', '.tsx', '.mjs', '.js', '.jsx', '.css'],
-		mainFields: ['svelte', 'browser', 'module', 'main'],
-	},
-	output: {
-		path: __dirname + '/../../../out/commands',
-		filename: '[name].js',
-		chunkFilename: '[name].[id].js',
-	},
-	devtool: argv.mode === 'production' ? false : 'source-map',
-	module: {
-		rules: [
-			{
-				test: /\.svelte$/i,
-				use: {
-					loader: 'svelte-loader',
-					options: {
-						emitCss: true,
-						hotReload: true,
-						preprocess: sveltePreprocess(),
+		resolve: {
+			alias: {
+				svelte: path.resolve(__dirname, '..', '..', '..', 'node_modules', 'svelte')
+			},
+			extensions: ['.svelte', '.ts', '.tsx', '.mjs', '.js', '.jsx', '.css'],
+			mainFields: ['svelte', 'browser', 'module', 'main'],
+		},
+		output: {
+			path: __dirname + '/../../../out/commands',
+			filename: '[name].js',
+			chunkFilename: isProduction ? '[id].js' : '[name].[id].js',
+		},
+		devtool: isProduction ? false : 'source-map',
+		module: {
+			rules: [
+				{
+					test: /\.svelte$/i,
+					use: {
+						loader: 'svelte-loader',
+						options: {
+							emitCss: true,
+							hotReload: true,
+							preprocess: sveltePreprocess(),
+						},
 					},
 				},
-			},
-			{
-				test: /\.css$/i,
-				use: [
-					'style-loader',
-					'css-loader',
-				],
-			},
-			{
-				include: /\.ts$/i,
-				loader: 'ts-loader',
-			},
-			{
-				test: /\.svg$/i,
-				loader: 'raw-loader',
-			},
-			{
-				test: /\.(png|jpe?g|gif)$/i,
-				loader: 'file-loader',
-				options: {
-					name: '[path][name].[ext]',
+				{
+					test: /\.css$/i,
+					use: [
+						'style-loader',
+						'css-loader',
+					],
 				},
-			},
-		],
-	},
+				{
+					include: /\.ts$/i,
+					loader: 'ts-loader',
+				},
+				{
+					test: /\.svg$/i,
+					loader: 'raw-loader',
+				},
+				{
+					test: /\.(png|jpe?g|gif)$/i,
+					loader: 'file-loader',
+					options: {
+						name: '[path][name].[ext]',
+					},
+				},
+			],
+		},
+	};
 };
